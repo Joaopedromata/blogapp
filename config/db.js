0ffc5c76372be1983e3a7661696de5f82b25a16c
Joaopedromata <62820737+Joaopedromata@users.noreply.github.com>
2020-05-09 11:14:05 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://joaopedromata:gqj7789@cluster0-4wzhi.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://joaopedromata:<password>@cluster0-4wzhi.mongodb.net/test?retryWrites=true&w=majority'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
